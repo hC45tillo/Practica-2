27c15342e595f3a869b6848f72f84fe350a608ef
Hugo <Castillo>
2020-03-07 06:48:28 -0600

    1.6 Camera Behaviour

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public GameObject target;
+    public float minHeight = 4.0f;
+    public float cameraHeightFactor = 1.5f;
+
+    private float height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(0, target.transform.position.y + height, 0);
+        height = minHeight + 2f * cameraHeightFactor;
+    }
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float lifes =  3f;
+    public float discoverdBases = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
